7a46e9a43fb17fb7c939f554704ef2904166d4e9
Ilia_Korol <iliyaking46@gmail.com>
2021-07-17 21:45:05 +0300

    some refactoring

--- server/index.js
+++ server/index.js
@@ -6,67 +6,58 @@ const data = require('./data.json')
 const app = express();
 const PORT = 5000
 
-// app.use(cors());
 app.use(bodyParser.json())
 
 app.use(express.static('build'));
 
-// app.get('/api/test', (res, req) => {
-//     req.send('test api')
-// })
-//
-// app.post('/api/test', (res, req) => {
-//     console.log(res.body)
-//     req.send('post test api')
-// })
-
 app.route('/api/categories')
-    .get((res, req) => {
+    .get((req, res) => {
         const categories = data.map(({type, name, imgSrc}) => ({type, name, imgSrc}))
-        req.json(categories)
+        res.json(categories)
     })
-    .post((res, req) => {
+    .post((req, res) => {
         const {type, name, imgSrc} = res.body
 
         if (!type || !name || !imgSrc) {
-            req.statusCode = 500;
-            return req.json({error: 'Wrong values: type, name or imgSrc'})
+            res.statusCode = 500;
+            return res.json({error: 'Wrong values: type, name or imgSrc'})
         }
 
         const newCategory = {type, name, imgSrc, items: []}
         data.push(newCategory)
-        req.json(newCategory)
+        res.json(newCategory)
     })
 
 app.route('/api/categories/:type')
-    .get((res, req) => {
-        const {type} = res.params;
+    .get((req, res) => {
+        const {type} = req.params;
         console.log(type)
         const cardType = data.find(category => category.type === type)
 
         if (!cardType) {
-            req.statusCode = 404
-            req.json({error: 'Category not found'})
+            res.statusCode = 404
+            res.json({error: 'Category not found'})
         }
 
-        req.json(cardType.items)
+        res.json(cardType.items)
 
     })
-    .post((res, req) => {
-        const {word, translation, image, soundSrc, type} = res.body
+    .post((req, res) => {
+        const {word, translation, image, soundSrc, type} = req.body
 
         if (!word || !translation || !image || !soundSrc || !type) {
-            req.statusCode = 500;
-            return req.json({error: 'Wrong values: word, translation, image, soundSrc or type'})
+            res.statusCode = 500;
+            return res.json({error: 'Wrong values: word, translation, image, soundSrc or type'})
         }
 
         const newCard = {word, translation, image, soundSrc}
         const cardTypeIndex = data.findIndex(category => category.type === type);
         data[cardTypeIndex].items.push(newCard)
-        req.json(newCard)
+        res.json(newCard)
     })
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/../build/index.html'))
 });
+
 app.listen(PORT, () => console.log(`Server is started at port localhost:${PORT}`))
